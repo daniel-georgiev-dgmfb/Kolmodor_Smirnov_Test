01b94252bbf56d66e4ca572f9cd539052e54f805
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-03-16 19:39:39 +0200

    foo

--- Transform_API/app.py
+++ Transform_API/app.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 def home():
     return ("Post readings to /specificFunction to get a result")
 
-@app.route('/stats/funcs/fft', methods = ['POST'])
+@app.route('/transform/funcs/fft', methods = ['POST'])
 def FourierTransforms():
     readings = request.form;
 
